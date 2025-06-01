4bade482891093901f6951839607c85471ae6765
Jonathan Grinstead <jonathangrinstead@Jonathans-MacBook-Pro-2.local>
2025-06-01 17:55:45 +0100

    Basic about me card finished

---
+++ src/components/AboutMeCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+</script>
+
+<template>
+  <Card>
+    <CardHeader class="flex justify-center">
+      <img src="@/assets/memoji.png" alt="memoji" class="w-40 h-40">
+    </CardHeader>
+    <CardContent>
+      <p class="text-sm text-center max-w-md mx-auto leading-relaxed">
+        Hey i'm <span class="text-primary">Jonathan</span>, a full stack software developer from the UK. Im interested in building intuitive web applications, music, photography and travel.
+      </p>
+    </CardContent>
+  </Card>
+</template>
+
+
+<style scoped>
+.text-primary {
+  background: linear-gradient(45deg, #3b82f6, #8b5cf6);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  font-weight: bold;
+  font-size: 1.6rem;
+}
+</style>
